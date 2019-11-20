d9cff7c915230138b5700a3d2dca73fe0f98e8e6
Jaafar Ben Abderrazak <jaafarbenabderrazak.info@gmail.com>
2019-11-20 09:28:48 +0100

    Create regular_expression.py

---
+++ regular_expression.py
@@ -0,0 +1,23 @@
+#find()
+for line in hand:
+    line = line.rstrip()
+    if line.find("From:") >= 0 :
+        print(line)
+        
+#re.search()  
+import re
+for line in hand:
+    line = line.rstrip()
+    if re.search("From:",line):
+        print(line)
+        
+#startswith() 
+for line in hand:
+    line = line.rstrip()
+    if line.startswith("From:") >= 0 :
+        print(line)
+
+#findall()
+sentence = "my 2 favorite numbers are 3 and 71"
+numbers = re.findall("[0-9]+",sentence)
+print(numbers)

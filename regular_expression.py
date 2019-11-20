f36f2e0cfc1eb381171609d68f8e017c350036c3
Jaafar Ben Abderrazak <jaafarbenabderrazak.info@gmail.com>
2019-11-20 09:49:52 +0100

    Update regular_expression.py

--- regular_expression.py
+++ regular_expression.py
@@ -1,23 +1,21 @@
-#find()
-for line in hand:
-    line = line.rstrip()
-    if line.find("From:") >= 0 :
-        print(line)
+line = "Regular expressions are a very specialized language that allow us to succinctly search strings and extract data from strings."
+
+#re.search()
+#This method takes a regular expression pattern and a string and searches for that pattern within the string. 
+print(re.search("data",line))
         
-#re.search()  
-import re
-for line in hand:
-    line = line.rstrip()
-    if re.search("From:",line):
-        print(line)
+#find()
+#This method determines if string str occurs in string, or in a substring of string if starting index beg and ending index end are given.
+print(line.find("data"))
         
 #startswith() 
-for line in hand:
-    line = line.rstrip()
-    if line.startswith("From:") >= 0 :
-        print(line)
+#This method returns true if found matching string otherwise false.
+print(line.startswith("data") >= 0)
 
 #re.findall()
+#findall() is probably the single most powerful function in the re module. 
+#Above we used re.search() to find the first match for a pattern. 
+#findall() finds *all* the matches and returns them as a list of strings, with each string representing one match.
 sentence = "my 2 favorite numbers are 3 and 71"
 numbers = re.findall("[0-9]+",sentence)
 print(numbers)
